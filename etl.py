703bc5f2aedf05f40f11d663559e1f64cb7d2d93
Kelly G Joseph <seisolo76@gmail.com>
2020-05-20 11:24:40 -0600

    Update etl.py
    added markdown comments

--- etl.py
+++ etl.py
@@ -6,6 +6,16 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    Perform ETL on song_data to create the songs and artists dimensional tables: 
+    - Process a single song data file and upload to database. 
+    - Extract song data + implement sql query to insert records into table.
+    - Extract artist data + implement sql query to insert records into table.
+    
+    Parameters:
+    - cur: cursor object that allows Python to execute PostgreSQL commands in a database session
+    - filepath (string) : path to a song_data file
+    """
     # open song file
     df = pd.read_json(filepath, lines = True)
 
@@ -20,6 +30,18 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """
+    Perform ETL on log_data to create the time and users dimensional tables and songplays fact table: 
+    - Process a single log data file and upload to database. 
+    - Extract log data + implement sql query to insert records into table.   
+    - Extract time data + implement sql query to insert records for the timestamps into table.
+    - Extract user data + implement sql query to insert records into table.
+    - Extract and inserts data for songplays table from different tables by implementing a select query.
+    
+    Parameters:
+    - cur: cursor object that allows Python to execute PostgreSQL commands in a database session
+    - filepath (string) : path to a log_data file
+    """
     # open log file
     df = pd.read_json(filepath, lines = True)
 
@@ -63,6 +85,15 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    ""
+    Process all files within the filepath directory through the input function.
+    
+    Parameters:
+    - cur: cursor object that allows Python to execute PostgreSQL commands in a database session
+    - conn: connection created to the database
+    - filepath (string) : path to data file
+    - func: process function
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -82,6 +113,13 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """
+    Build ETL Pipeline for Sparkify song play data:
+    
+    Open a session to the Postgres database, 
+    acquire a cursor object to process SQL queries,
+    and process both the song and the log data.    
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
